0b0cbe258931f888e2fc254eb0daff5ce3e1328b
elon bashari <elon.bashari@live.biu.ac.il>
2019-01-07 18:46:03 +0200

    t

--- ex2_py.py
+++ ex2_py.py
@@ -1,3 +1,5 @@
+import math
+
 import utils as ut
 import decision_tree as dt
 import knn
@@ -5,8 +7,8 @@ import nb
 
 
 if __name__ == '__main__':
-    _, data, tags = ut.parse_data("data\\train.txt", tagged_data=True)
-    _, test, golden_tags = ut.parse_data("data\\test.txt", tagged_data=True)
+    _, data, tags = ut.parse_data("train.txt", tagged_data=True)
+    _, test, golden_tags = ut.parse_data("test.txt", tagged_data=True)
 
     tree = dt.train(data, tags, list(data[0].keys()))
     fd = open("output_tree.txt", "w")
@@ -19,6 +21,8 @@ if __name__ == '__main__':
     fd.write("\t".join(["Num","DT","KNN","NB"]) + "\n")
     for i,(t,k,n) in enumerate(zip(tree_pred, knn_pred, nb_pred)):
         fd.write("\t".join([str(i+1),t,k,n]) + "\n")
-    fd.write("\t".join([" ", "{0:.2f}".format(tree_acc), "{0:.2f}".format(knn_acc), "{0:.2f}".format(nb_acc)]))
+    acc = [tree_acc, knn_acc, nb_acc]
+    acc = [str(math.ceil(accuracy)/100) for accuracy in acc]
+    fd.write("\t".join([" "] + acc))
     fd.close()
 
